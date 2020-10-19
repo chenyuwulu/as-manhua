464cbc947b521f0289d12d411ef2e2711c33fad1
chencailong <chencailong@chargefrog.com>
2020-10-19 17:24:43 +0800

    修改名称

--- chenyu_demo/src/main/java/com/chenyuwulu/chenyu_demo/Course_1.kt
+++ chenyu_demo/src/main/java/com/chenyuwulu/chenyu_demo/Course_1.kt
@@ -1,34 +1,13 @@
 package com.chenyuwulu.chenyu_demo
 
-import android.content.Context
 import android.os.Bundle
-import android.util.AttributeSet
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatTextView
 
-
-class Course_1 : AppCompatActivity() {
+class Course_2 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.course_1)
-        title = "TextView展示页面"
-
-    }
-    class MyTextView : AppCompatTextView {
-        constructor(context: Context?) : super(context!!) {}
-        constructor(context: Context?, attrs: AttributeSet?) : super(
-            context!!, attrs
-        ) {
-        }
-
-        constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
-            context!!, attrs, defStyleAttr
-        ) {
-        }
-
-        override fun isFocused(): Boolean {  //此方法直接返回true
-            return true
-        }
+        setContentView(R.layout.course_2)
+        title = "EditView展示页"
     }
 }
