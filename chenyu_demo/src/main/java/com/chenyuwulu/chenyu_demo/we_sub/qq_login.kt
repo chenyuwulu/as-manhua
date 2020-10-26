fbef0755a40e186bc1df6891614c0a90a6604fa9
chencailong <chencailong@chargefrog.com>
2020-10-26 18:56:46 +0800

    优化QQ登录功能

--- chenyu_demo/src/main/java/com/chenyuwulu/chenyu_demo/we_sub/qq_login.kt
+++ chenyu_demo/src/main/java/com/chenyuwulu/chenyu_demo/we_sub/qq_login.kt
@@ -3,8 +3,10 @@ package com.chenyuwulu.chenyu_demo.we_sub
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.chenyuwulu.chenyu_demo.R
 import com.tencent.connect.UserInfo
 import com.tencent.connect.common.Constants
@@ -15,12 +17,12 @@ import kotlinx.android.synthetic.main.we_sub_qq_login.*
 import org.json.JSONException
 import org.json.JSONObject
 
-
 class qq_login : AppCompatActivity() {
 
     private val TAG = "qq_login"
     private lateinit var  mTencent: Tencent
     private var mUserInfo: UserInfo? = null
+    private lateinit var image : ImageView
     private lateinit var mIUiListener:IUiListener
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -36,6 +38,7 @@ class qq_login : AppCompatActivity() {
 //            调起QQ小程序
 //            mTencent.startMiniApp(this, "1109689877", "pages/index/index", "release")
             mTencent.login(this, "all", mIUiListener)
+
         }
     }
 
@@ -59,9 +62,16 @@ class qq_login : AppCompatActivity() {
                         Log.e(TAG, "登录成功$response")
                         //登录成功后进行Gson解析即可获得你需要的QQ头像和昵称
                         // Nickname  昵称
+                        val we_obj = response as JSONObject
+                        Toast.makeText(this@qq_login, we_obj.getString("nickname"), Toast.LENGTH_SHORT).show()
+//                        Glide.with(this@qq_login).load(we_obj.getString("figureurl_qq")).into(this@qq_login.iv_icon);
                         //Figureurl_qq_1 //头像
-                    }
+                        loadNetImage(we_obj.getString("figureurl_qq"))
 
+                    }
+                    fun loadNetImage(src : String){
+                        Glide.with(this@qq_login).load(src).into(this@qq_login.iv_icon)
+                    }
                     override fun onError(uiError: UiError) {
                         Log.e(TAG, "登录失败$uiError")
                     }
@@ -92,5 +102,4 @@ class qq_login : AppCompatActivity() {
         }
         super.onActivityResult(requestCode, resultCode, data)
     }
-
 }
