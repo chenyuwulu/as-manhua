a932a1b0db6feaba6a43bd10af76cbdaec7c2c7e
chencailong <chencailong@chargefrog.com>
2020-11-05 18:09:58 +0800

    更新至intent

---
+++ chenyu_demo/src/main/java/com/chenyuwulu/chenyu_demo/Course_44.kt
@@ -0,0 +1,12 @@
+package com.chenyuwulu.chenyu_demo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Course_44 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.course_44)
+        title = "intent"
+    }
+}
